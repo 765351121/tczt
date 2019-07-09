cabac3f97692e2dad76486c6bd5174b7f5ca8f99
tenon <765351121@qq.com>
2019-07-09 16:44:08 +0800

    story# copyright info

--- src/components/GlobalFooter/GlobalFooter.vue
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="wrap">
-    <div class="content">
+  <div>
+    <div class="wrap">
+      <div class="content">
+        <div>
+          <div><img src="@/assets/logo/logo_trans.png" alt=""></div>
+          <p>客服热线:</p>
+          <p>400-085-6566</p>
+          <p>工作日    9:00-18:00</p>
+        </div>
+        <div>
+          <div>
+            <img src="@/assets/qrcode/1.png" alt="">
+            <span>关注官方微博</span>
+          </div>
+          <div>
+            <img src="@/assets/qrcode/2.png" alt="">
+            <span>关注微信订阅号</span>
+          </div>
+          <div>
+            <img src="@/assets/qrcode/3.png" alt="">
+            <span>关注微信服务号</span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <span>关于我们</span>
+              <span>安全保障</span>
+              <span>帮助中心</span>
+            </div>
+            <div>
+              <span>公司简介</span>
+              <span>资金存管</span>
+              <span>出借问题</span>
+            </div>
+            <div>
+              <span>组织信息</span>
+              <span>风险控制</span>
+              <span>充值提现</span>
+            </div>
+            <div>
+              <span>法律声明</span>
+              <span>合规报告</span>
+              <span>账户管理</span>
+            </div>
+          </div>
+        </div>
+      </div>  
+    </div>
+    <div class="records">
       <div>
-        <div><img src="@/assets/logo/logo_trans.png" alt=""></div>
-        <p>客服热线:</p>
-        <p>400-085-6566</p>
-        <p>工作日    9:00-18:00</p>
+        <img src="@/assets/icons/beian.png" alt="" width="20px" height="20px">
+        <span>浙公网安备 0000000000000号</span>
       </div>
       <div>
-        <div>
-          <img src="@/assets/qrcode/1.png" alt="">
-          <span>关注官方微博</span>
-        </div>
-        <div>
-          <img src="@/assets/qrcode/2.png" alt="">
-          <span>关注微信订阅号</span>
-        </div>
-        <div>
-          <img src="@/assets/qrcode/3.png" alt="">
-          <span>关注微信服务号</span>
-        </div>
+        <img src="@/assets/icons/copyrigh.png" alt="" width="20px" height="20px">
+        <span>浙ICP备00000000号-1</span>
       </div>
-      <div>
-        <div>
-          <div>
-            <span>关于我们</span>
-            <span>安全保障</span>
-            <span>帮助中心</span>
-          </div>
-          <div>
-            <span>公司简介</span>
-            <span>资金存管</span>
-            <span>出借问题</span>
-          </div>
-          <div>
-            <span>组织信息</span>
-            <span>风险控制</span>
-            <span>充值提现</span>
-          </div>
-          <div>
-            <span>法律声明</span>
-            <span>合规报告</span>
-            <span>账户管理</span>
-          </div>
-        </div>
-      </div>
-    </div>  
+      <div>浙ICP证B2-000000000</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'GlobalFooter'
-}
+  name: "GlobalFooter"
+};
 </script>
 
 <style lang="less" scoped>
@@ -69,7 +82,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  &>div {
+  & > div {
     flex: 1;
     p {
       margin-bottom: 0;
@@ -96,7 +109,7 @@ export default {
       display: flex;
       justify-content: space-around;
       align-items: center;
-      &>div {
+      & > div {
         flex: 1;
         text-align: center;
         span {
@@ -111,9 +124,9 @@ export default {
     &:nth-child(3) {
       display: flex;
       justify-content: flex-end;
-      &>div {
+      & > div {
         width: 280px;
-        &>div {
+        & > div {
           display: flex;
           justify-content: space-between;
           align-items: center;
@@ -145,6 +158,27 @@ export default {
   }
 }
 
+.records {
+  min-width: 1200px;
+  overflow-x: visible;
+  background-color: #000;
+  height: 50px;
+  font-size: 14px;
+  color: #939393;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &>div {
+    padding-left: 20px;
+    span {
+      display: inline-block;
+      padding-left: 5px;
+    }
+    &:not(:last-child) {
+      cursor: pointer;
+    }
+  }
+}
 
 </style>
 
