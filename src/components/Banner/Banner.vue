a57126c709b969a7a9f16ab45450780bdb2451d0
tenon <765351121@qq.com>
2019-07-09 13:17:05 +0800

    story# banner

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrap">
+    <a-carousel autoplay>
+      <div><img src="@/assets/banner/1.png" alt=""></div>
+      <div><img src="@/assets/banner/2.png" alt=""></div>
+      <div><img src="@/assets/banner/3.png" alt=""></div>
+      <div><img src="@/assets/banner/4.png" alt=""></div>
+    </a-carousel>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  namer: "banner"
+};
+</script>
+
+<style lang="less" scoped>
+.wrap {
+  min-width: 1200px;
+  overflow-x: visible;
+
+  :global {
+    .ant-carousel .slick-slide img {
+      width: 100%;
+      height: auto;
+      
+    }
+    // .ant-carousel >>> .slick-slide {
+    //   text-align: center;
+    //   height: 160px;
+    //   line-height: 160px;
+    //   background: #364d79;
+    //   overflow: hidden;
+    // }
+
+    // .ant-carousel >>> .slick-slide h3 {
+    //   color: #fff;
+    // }
+  }
+
+
+}
+
+</style>
+
+
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -2,6 +2,10 @@
   <div class="root-wrap">
     <GlobalHeader />
     <GlobalNav />
+    <Banner />
+    <div class="content">
+      111
+    </div>
     
 
 
@@ -12,12 +16,14 @@
 <script>
 import GlobalHeader from '@/components/GlobalHeader'
 import GlobalNav from '@/components/GlobalNav'
+import Banner from '@/components/Banner'
 
 export default {
   name: 'basiclayout',
   components: {
     GlobalHeader,
     GlobalNav,
+    Banner,
   }
 }
 </script>
@@ -29,6 +35,11 @@ export default {
     height: 40px;
     line-height: 40px;
     overflow: visible;
+  }
+  .content {
+    border: 1px solid #0ff;
+    width: 1200px;
+    margin: 0 auto;
   }
 </style>
 
