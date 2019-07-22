d1c78d3c1694718acca74a9f672db119669eef15
tenon <765351121@qq.com>
2019-07-22 17:54:21 +0800

    story# stash send sms code

--- src/views/user/components/Register.vue
+++ src/views/user/components/Register.vue
@@ -9,17 +9,32 @@
           </a-form-item>
         </a-form>
 
-        <a-form>
-          <a-form-item>
-            <a-input placeholder="请输入图形验证码"/>
-          </a-form-item>
-        </a-form>
+        <div class="imgvc-wrap">
+          <a-form>
+            <a-form-item>
+              <a-input placeholder="请输入图形验证码"/>
+            </a-form-item>
+          </a-form>
+          <span class="imgvc" @click="getImgvc">
+            <img :src="require(`@/assets/images/imgvc/${state.imgvc}.jpg`)" alt>
+          </span>
+        </div>
 
-        <a-form>
-          <a-form-item>
-            <a-input placeholder="请输入短信验证码"/>
-          </a-form-item>
-        </a-form>
+        <div class="sms-code-wrap">
+          <a-form>
+            <a-form-item>
+              <a-input placeholder="请输入短信验证码"/>
+            </a-form-item>
+          </a-form>
+          <span class="sms-code-btn">
+            <a-button 
+              block 
+              @click="handleSmsCode"
+              :disabled="!!state.count"
+              v-text="!state.count && state.tips || (`${state.count}s后可重发`)"
+            ></a-button>
+          </span>
+        </div>
 
         <a-form>
           <a-form-item>
@@ -49,7 +64,46 @@
 
 <script>
 export default {
-  name: "T-register"
+  name: "T-register",
+  data() {
+    return {
+      state: {
+        imgvc: 1,
+        imgArr: new Array(7).join(),
+        count: 0,
+        tips: '获取验证码',
+      }
+    };
+  },
+  methods: {
+    getImgvc() {
+      // mock for getImgvc
+      let rand = Math.floor(Math.random() * this.state.imgArr.length) + 1;
+      const { imgvc } = this.state;
+      if (rand == imgvc) {
+        return this.getImgvc();
+      }
+      return (this.state.imgvc = rand);
+    },
+    smsCountDown() {
+      let count = 60;
+      this.state.count = count;
+      this.interval = setInterval(() => {
+        count -= 1;
+        this.state.count = count;
+        if (count === 0) {
+          this.state.tips = '重新发送';
+          clearInterval(this.interval);
+        }
+      }, 1000);
+    },
+    handleSmsCode() {
+      console.log('..........')
+      // check form 
+      // count down
+      this.smsCountDown()
+    }
+  }
 };
 </script>
 
@@ -91,6 +145,47 @@ export default {
   margin-top: 15px;
   text-align: center;
 }
+
+.imgvc-wrap {
+  position: relative;
+  .imgvc {
+    display: inline-block;
+    width: 100px;
+    height: 36px;
+    line-height: 36px;
+    cursor: pointer;
+    img {
+      width: 100px;
+      height: auto;
+    }
+    position: absolute;
+    top: -10px;
+    right: 0;
+  }
+}
+
+.sms-code-wrap {
+  position: relative;
+  .sms-code-btn {
+    display: inline-block;
+    width: 100px;
+    height: 32px;
+    line-height: 32px;
+    position: absolute;
+    top: -5px;
+    right: 0;
+    button {
+      border: none;
+      color: #0181fe;
+      background-color: transparent!important;
+    }
+    /deep/ [ant-click-animating-without-extra-node]:after, .ant-click-animating-node {
+      display: none;
+    }
+  }
+}
+
+
 </style>
 
 
