ed656490438fbaaf3821b87bba18740dc0f735c5
tenon <765351121@qq.com>
2019-07-26 11:38:51 +0800

    story# stash filter ui

--- src/views/product/List.vue
+++ src/views/product/List.vue
@@ -1,34 +1,73 @@
 <template>
   <div>
     <div class="wrap">
-      <ScatTitle />
+      <ScatTitle/>
+      <div class="item-list">项目列表</div>
+      <div class="filter-wrap">
+        <div class="filter loan-time-wrap">
+          <div class="title">出借期限</div>
+          <div class="action-wrap">111</div>
+        </div>
+
+        <div class="filter product-status-wrap">
+          <div class="title">项目状态</div>
+          <div class="action-wrap">222</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import ScatTitle from './components/List/ScatTitle'
+import ScatTitle from "./components/List/ScatTitle";
 
 export default {
-  name: 'T-product-list',
+  name: "T-product-list",
   components: {
-    ScatTitle,
+    ScatTitle
   },
   data() {
-    return {
-      
-    }
-  },
-}
+    return {};
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .wrap {
-//  border: 1px solid #0ff;
+  //  border: 1px solid #0ff;
+}
+.item-list {
+  color: #333;
+  font-size: 20px;
+  font-family: "Microsoft YaHei";
+  margin: 20px 0;
 }
 
+.filter-wrap {
+  border: 1px solid #f0f;
+  .filter {
+    display: flex;
+    justify-content: flex-start;
+    height: 55px;
+    line-height: 55px;
+    .title {
+      background-color: #f4f5f6;
+      width: 200px;
+      text-align: center;
+      font-size: 14px;
+      font-family: "Microsoft YaHei";
+      color: #333;
+    }
+    .action-wrap {
+      border: 1px solid #0ff;
+      flex: 1;
+      background-color: #fff;
+    }
+  }
+}
 
-
+.loan-time-wrap {
+}
 </style>
 
 
