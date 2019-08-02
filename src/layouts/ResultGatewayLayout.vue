200f394228ee367625533d847d691881c4dadaf5
tenon <765351121@qq.com>
2019-08-02 13:31:28 +0800

    story# loading animate

--- src/layouts/ResultGatewayLayout.vue
+++ src/layouts/ResultGatewayLayout.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="root">
-    <div>loading</div>
+    <div class="header-wrap">
+      <div class="header-content">
+        <img src="@/assets/logo/logo_blue.png" alt>
+        <span>|</span>
+        <img src="@/assets/logo/logo_xw.png" alt>
+      </div>
+    </div>
+    <div class="content-wrap">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'T-result-gateway-layout',
-
-}
+  name: "T-result-gateway-layout"
+};
 </script>
 
-
 <style lang="less" scoped>
-.root {
-
+.header-wrap {
+  background-color: #fff;
 }
-
-
-
+.header-content {
+  width: 1200px;
+  height: 100px;
+  line-height: 100px;
+  margin: 0 auto;
+  background-color: #fff;
+  & > span {
+    display: inline-block;
+    padding: 0 10px;
+    color: rgba(0, 0, 0, 0.45);
+  }
+}
+.content-wrap {
+  width: 1200px;
+  height: 640px;
+  background-color: #fff;
+  margin: 0 auto;
+  margin-top: 30px;
+}
 </style>
 
--- src/views/result/gateway/Loading.vue
+++ src/views/result/gateway/Loading.vue
@@ -1,5 +1,105 @@
 <template>
-  <div>
-    111
+  <div class="wrap">
+    <div class="content-wrap">
+      <div class="loading-wrap">
+        <svg class="svg">
+          <circle cx="65" cy="65" r="60" class="fixed_circle"></circle>
+          <circle cx="-65" cy="65" r="60" class="ani_circle"></circle>
+        </svg>
+        <span class="count-donw">{{ count }}s</span>
+      </div>
+      <div class="tis-wrap">
+        <p>处理中，等待银行返回结果</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "T-result-gateway-loading",
+  data() {
+    return {
+      count: 10,
+    };
+  },
+  methods: {
+    initCountDown() {
+      let count = 10;
+      this.interval = setInterval(() => {
+        count -= 1;
+        this.count = count;
+        if (count === 0) {
+          clearInterval(this.interval);
+          //clearInterval(this.isDoneInterval);
+        }
+      }, 1000);
+    }
+  },
+  mounted () {
+    this.initCountDown()
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wrap {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.content-wrap {
+  text-align: center;
+  .loading-wrap {
+    position: relative;
+    .count-donw {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      font-size: 24px;
+      color: #333;
+    }
+  }
+  .tis-wrap {
+    margin-top: 40px;
+    & > p {
+      font-size: 20px;
+      margin-bottom: 0px;
+    }
+  }
+}
+.svg {
+  width: 130px;
+  height: 130px;
+  .fixed_circle {
+    fill: #ffffff;
+    stroke: #508df8;
+    stroke-width: 3px;
+  }
+  @-webkit-keyframes wind_Frames {
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+  @keyframes wind_Frames {
+    to {
+      stroke-dashoffset: 0;
+    }
+  }
+  .ani_circle {
+    stroke: #eceef0;
+    stroke-width: 4px;
+    fill: transparent;
+    stroke-dasharray: 380;
+    stroke-dashoffset: 380;
+    -webkit-animation: wind_Frames 10s forwards linear;
+    animation: wind_Frames 10s forwards linear;
+    -webkit-transform: rotate(-90deg);
+    transform: rotate(-90deg);
+  }
+}
+</style>
+
