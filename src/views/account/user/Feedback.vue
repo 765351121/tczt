b03ac7be3e4ca1e50be195081b09847125e84f54
tenon <765351121@qq.com>
2019-08-08 16:11:53 +0800

    story# account feedback ui

--- src/views/account/user/Feedback.vue
+++ src/views/account/user/Feedback.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
-    <div>
-      feedback
+    <div class="wrap">
+      <p class="title">意见反馈</p>
+      <div class="form-wrap">
+        <a-textarea maxlength="500" placeholder="请输入不少于10个字的描述…………" :rows="10"/>
+      </div>
+    </div>
+    <div class="btn-wrap">
+      <a-button type="primary">提交</a-button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "T-account-feedback"
+};
+</script>
+
+<style lang="less" scoped>
+.title {
+  font-size: 18px;
+  color: #333;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 20px;
+}
+.btn-wrap {
+  margin-top: 30px;
+}
+</style>
